d4d118778e5831b98d1be3e12ffaa945f84cbb54
Elijah59yf <eakinseloyin@gmail.com>
2026-08-31 23:03:43 +0000

    Notifications Working

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     id("com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    // Requires android/app/google-services.json to be present - see
+    // the push notification setup note. The build fails without it
+    // once this plugin is applied, so don't add this file's presence
+    // check here; it's meant to fail loudly and early instead.
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -12,27 +17,27 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     defaultConfig {
-   	 // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-   	 applicationId = "com.monarch.parchment"
-   	 // You can update the following values to match your application needs.
-   	 // For more information, see: https://flutter.dev/to/review-gradle-config.
-   	 minSdk = flutter.minSdkVersion
-   	 targetSdk = flutter.targetSdkVersion
-   	 // Uses the version code from pubspec.yaml. When using split APKs, 1000 * ABI_VERSION
-   	 // is added automatically by Flutter. (https://developer.android.com/studio/build/configure-apk-splits#configure-APK-vers>
-   	 // You can force using the value of versionCode by specifying the `-P force-version-code-ignoring-abi=true`
-   	 // flag during build.
-   	 versionCode = flutter.versionCode
-   	 versionName = flutter.versionName
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.monarch.parchment"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        // Uses the version code from pubspec.yaml. When using split APKs, 1000 * ABI_VERSION
+        // is added automatically by Flutter. (https://developer.android.com/studio/build/configure-apk-splits#configure-APK-versions)
+        // You can force using the value of versionCode by specifying the `-P force-version-code-ignoring-abi=true`
+        // flag during build.
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
 
-   	 // ADD THIS BLOCK:
-   	 ndk {
-        	abiFilters += setOf("arm64-v8a")
-   	 }
-     }
+        ndk {
+            abiFilters += setOf("arm64-v8a")
+        }
+    }
 
     buildTypes {
         release {
@@ -47,6 +52,10 @@ kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
